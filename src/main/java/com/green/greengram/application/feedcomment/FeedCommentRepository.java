35e743f70f0e92fcddc95e58826aa6eb2b4cc3c8
qwazsx1346 <qwazsx1346@gmail.com>
2026-04-01 12:16:04 +0900

    댓글 리스트 가져오기 학습중

--- src/main/java/com/green/greengram/application/feedcomment/FeedCommentRepository.java
+++ src/main/java/com/green/greengram/application/feedcomment/FeedCommentRepository.java
@@ -1,8 +1,22 @@
 package com.green.greengram.application.feedcomment;
 
+import com.green.greengram.application.feedcomment.model.FeedCommentGetRes;
 import com.green.greengram.entity.FeedComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 //<어떤 엔터티랑 연결하냐, 그 엔터티의 PK타입이 무엇이냐?>
+//from 에는 테이블명이 아닌 엔터티 이름을 적어줌, 한줄 적을때마다 뒤에 빈칸을 꼭 넣어주어야함
+//FeedCommentGetRes에 createdAt 타입을 LocalDateTime으로 바꿔줘야 빨간줄이 안뜸
+//FeedCommentGetRes에 적인 순서대로 적어주어야 함.
 public interface FeedCommentRepository extends JpaRepository<FeedComment, Long> {
+    @Query(" SELECT new com.green.greengram.application.feedcomment.model.FeedCommentGetRes(fc.id, fc.feed.id, fc.comment, u.id, u.nm, u.pic, fc.createdAt) " +
+            " FROM FeedComment fc JOIN fc.user u " +
+            " WHERE fc.feed.id = :feedId " +
+            " ORDER BY fc.feed.id DESC")
+    List<FeedCommentGetRes> getFeedCommentList(@Param("feedId") Long feedId, Pageable pageable);
 }
--- src/main/java/com/green/greengram/application/feedcomment/model/FeedCommentGetRes.java
+++ src/main/java/com/green/greengram/application/feedcomment/model/FeedCommentGetRes.java
@@ -1,12 +1,16 @@
 package com.green.greengram.application.feedcomment.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
+@AllArgsConstructor //생성자가 있어야해서 붙여줌. 아래 7개를 받는 생성자가 만들어짐.
 public class FeedCommentGetRes {
     @JsonProperty("feedCommentId")
     private long id;
@@ -16,5 +20,5 @@ public class FeedCommentGetRes {
     @JsonProperty("writerNickName")
     private String writerNm;
     private String writerPic;
-    private String createdAt;
+    private LocalDateTime createdAt;
 }
